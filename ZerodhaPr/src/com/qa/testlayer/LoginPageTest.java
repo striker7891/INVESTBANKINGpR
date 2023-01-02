8d26f1c8c4de7a6de8931edb9d5bb6aa38755410
VinayakG777 <vinayakghumare08@gmail.com>
2023-01-02 17:51:36 +0530

    loginpageFeature

---
+++ ZerodhaPr/src/com/qa/testlayer/LoginPageTest.java
@@ -0,0 +1,9 @@
+package com.qa.testlayer;
+
+public class LoginPageTest {
+	public static void main(String args[])
+	{
+		System.out.println(" login test");
+	}
+
+}
---
+++ ZerodhaPr/src/com/qa/utilities/Utilityclass.java
@@ -0,0 +1,9 @@
+package com.qa.utilities;
+
+public class Utilityclass {
+	public static void main(String args[])
+	{
+		System.out.println("utility class");
+	}
+
+}
